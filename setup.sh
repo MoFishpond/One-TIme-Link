f629295873ba98508c833a3b9815ac6e462dbccc
Yiming Gong <277645526@qq.com>
2022-07-09 23:25:19 -0400

    add more setup scripts

--- setup.sh
+++ setup.sh
@@ -14,4 +14,22 @@ then
   curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
 fi
+
+if ! [ -x "$(command -v java)" ]
+then
+  echo "Set up Java8..."
+  sudo apt-get update && sudo apt-get upgrade
+  sudo apt-get openjdk-8-jdk -y
+fi
+
+if ! [ -x "$(command -v mvn)" ]
+then
+  echo "Set up Maven..."
+  wget https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz
+  tar -xzvf apache-maven-3.8.6-bin.tar.gz
+  source setup_maven.sh
+fi
+
+echo "Build project..."
+cd backend && mvn clean package && cd ..
 docker-compose up -d
